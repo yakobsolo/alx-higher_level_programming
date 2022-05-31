7ae2df4dd48fb48c485a8a3393265aa8f51aeab2
user name <user email>
2022-05-31 20:06:46 +0000

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,8 +5,6 @@ def fizzbuzz():
             print("Fizz", end='')
         elif i % 5 == 0:
             print("Buzz", end='')
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end="")
-        else:
-           print("{:d}".format(i), end='')
+        elif i % 3 and i % 5:
+            print("{:d}".format(i), end='')
         print(end=' ')
