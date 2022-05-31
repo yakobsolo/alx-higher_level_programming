7478c03a7d4404fcac62236cc11f73f05965a7d6
user name <user email>
2022-05-31 09:46:55 +0000

    correction

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number < 0:
-    print("is negative")
+    print(number, "is negative")
 elif number > 0:
-    print("is postive")
+    print(nubmer, "is postive")
 else:
-    print("is zero")
+    print(number, "is zero")
