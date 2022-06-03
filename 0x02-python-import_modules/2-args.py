db28623f9a3d41b83050cc7f699cdbd4dc6f7426
yakob <99880602+yakobsolo@users.noreply.github.com>
2022-06-04 00:15:11 +0300

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("{} argument.".format(0))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+num_len = len(sys.argv[1:])
+i = 1
+while num_len >= i:
+    print("{}: {}".format(i, sys.argv[i]))
+    i += 1
