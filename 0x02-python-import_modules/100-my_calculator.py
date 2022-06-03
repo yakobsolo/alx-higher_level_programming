4edc233cbcde6a7e6089f5e7cb150db85d7f5da9
user name <user email>
2022-06-03 19:34:56 +0000

    task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+alt = ["+", "-", "*", "/"]
+function = [add, sub, mul, div]
+x = int(sys.argv[1])
+y = int(sys.argv[3])
+for i, j in enumerate(alt):
+    if sys.argv[2] == j:
+        print("{} {} {} = {}".format(x, j, y, function[i](x, y)))
+        break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
