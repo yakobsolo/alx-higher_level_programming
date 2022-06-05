3ce6fc8e7d7331db9f54cbebcb649eed9b72521c
user name <user email>
2022-06-05 08:49:11 +0000

    function that prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{}".format(j), end=" ")
-        print("")
+        print(" ".join("{}".format(j) for j in i))
