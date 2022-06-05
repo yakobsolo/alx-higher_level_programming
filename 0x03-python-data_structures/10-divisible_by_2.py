c14d96ff0d8001e62a00289b4cf5a3d9c893b19e
user name <user email>
2022-06-05 12:23:49 +0000

    function that finds all myltiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        bol_lis = []
+        for i in my_list:
+            if i % 2 == 0:
+                bol_lis.append(True)
+            else:
+                bol_lis.append(False)
+    return bol_lis
