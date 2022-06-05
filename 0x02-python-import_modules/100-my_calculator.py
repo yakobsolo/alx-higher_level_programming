e0ebb25551944db1f5e1a6408b6091404947bed3
user name <user email>
2022-06-05 14:34:07 +0000

    correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+from sys import argv
 from calculator_1 import add, sub, mul, div
-import sys
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+
+ops = ['+', '-', '*', '/']
+argc = len(argv)
+
+if __name__ == '__main__':
+    if (argc - 1) < 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    alt = ["+", "-", "*", "/"]
-    function = [add, sub, mul, div]
-    x = int(sys.argv[1])
-    y = int(sys.argv[3])
-    for i, j in enumerate(alt):
-        if sys.argv[2] == j:
-            print("{} {} {} = {}".format(x, j, y, function[i](x, y)))
-            break
+    elif argv[2] not in ops:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    else:
+        a, b = int(argv[1]), int(argv[3])
+        if argv[2] == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
