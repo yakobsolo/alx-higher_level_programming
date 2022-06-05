8ea4e730d4184fb2f527eff8b4c6cbcd8990d25b
user name <user email>
2022-06-05 14:17:32 +0000

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("{} argument.".format(0))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-num_len = len(sys.argv[1:])
-i = 1
-while num_len >= i:
-    print("{}: {}".format(i, sys.argv[i]))
-    i += 1
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("{} argument.".format(0))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        num_len = len(sys.argv[1:])
+        i = 1
+        while num_len >= i:
+            print("{}: {}".format(i, sys.argv[i]))
+            i += 1
