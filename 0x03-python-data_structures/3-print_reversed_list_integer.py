cbc78d5280e5ca946e76034582132c4514a51818
user name <user email>
2022-06-05 06:30:39 +0000

    function that prints all integers of alist in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        i = 1
-        while i <= len(my_list):
-            print("{}".format(my_list[-i]))
-            i += 1
+        for elm in my_list[::-1]:
+            print("{:d}".format(elm))
