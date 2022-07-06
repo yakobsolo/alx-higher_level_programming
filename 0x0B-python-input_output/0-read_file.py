2391f664994fa6362ad1cc761ddf9bf67539c149
user name <user email>
2022-07-06 09:29:10 +0000

    function that reads a text file print it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" 0-read_file: def read_file """
+
+
 def read_file(filename=""):
+    """
+        reads a text file (UTF-8) and prints to stdout
+    """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
