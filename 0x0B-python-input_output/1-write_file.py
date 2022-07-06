8bc5fd5fe89419d591697f652ba16186627a1cb4
user name <user email>
2022-07-06 14:52:37 +0000

    function that wites a string to a text and returns the number of the string

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" 1-write_file: def write_file """
+
+
 def write_file(filename="", text=""):
+    """
+        writes a string to a text file (UTF8)
+        Return:
+            no of characters written
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
