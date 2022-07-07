fa557711bbb763a9be9cac984223ddb67f314688
user name <user email>
2022-07-07 16:40:15 +0000

    convert to json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import json
-"""
-3-to_json_string: to_json_string
-"""
+""" 3-to_json_string: to_json_string """
 
 
 def to_json_string(my_obj):
     """
-    Return:
-        returns the json file
-        """
+        Return:
+            returns the json file
+        Args:
+            my_obj: ready object to convert to json string
+    """
     return json.dumps(my_obj)
