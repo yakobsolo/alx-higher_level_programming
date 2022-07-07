dada12c6a4e14ef837f0d3b43af83735debfb731
user name <user email>
2022-07-07 16:35:28 +0000

    convert to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""
+3-to_json_string: to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Return:
+        returns the json file
+        """
+    return json.dumps(my_obj)
