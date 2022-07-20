87bbd83de35719d52e12213e97c68e9022c100d9
user name <user email>
2022-07-20 09:24:04 +0000

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+""" 1-search_replace: def search_replace """
 def search_replace(my_list, search, replace):
+    """
+        search and replace a value
+        Args:
+            my_list:
+            search:
+            replace:
+        Return: 
+            the newly replaced list
+    """
     new_list = list(my_list)
     for x in range(len(my_list)):
         if my_list[x] == 2:
