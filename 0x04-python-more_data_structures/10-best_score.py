6e5bf5c22c644177202bbcd0a7e94c91bf578e5d
user name <user email>
2022-07-21 08:10:52 +0000

    functon that returns a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return sorted(a_dictionary.keys())[-1]
+    maxi = max(a_dictionary, key=lambda x: a_dictionary[x])
+    return maxi
