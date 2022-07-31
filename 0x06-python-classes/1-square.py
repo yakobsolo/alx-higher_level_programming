fd69204b94ab3c548ccd2dae56341831952a4d93
yakobsolo <yakisolo2009@gmail.com>
2022-07-31 15:33:07 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 1-square.py - Square """
+
+
+class Square:
+    """ square class private size """
+    def __init__(self, size):
+        """ private size
+        Args:
+        size: accept a value
+        """
+        self.__size = size
