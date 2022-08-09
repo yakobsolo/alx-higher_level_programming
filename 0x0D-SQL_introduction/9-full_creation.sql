4304e6c9dce929c8d9347597d4ff163df521c681
yakobsolo <yakisolo2009@gmail.com>
2022-08-09 18:25:59 +0000

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- full creation
 -- create a table
 CREATE TABLES second_table(id INT, name VARCHAR(256), score INT);
--- insert an entity
 INSERT INTO second_table(id, name, score) VALUES(1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
