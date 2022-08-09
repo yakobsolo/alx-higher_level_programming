97e8bab2e0ee9f141f880da050df3029bf947b72
yakobsolo <yakisolo2009@gmail.com>
2022-08-09 19:41:51 +0000

    group by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- number by score
 -- selects and group by score
-SELECT score, COUNT(*) AS "number" FROM second_table GROUP BY score ORDER BY "number" DESC;
+SELECT score, COUNT(*) AS "number" FROM second_table
+GROUP BY score ORDER BY "number" DESC;
