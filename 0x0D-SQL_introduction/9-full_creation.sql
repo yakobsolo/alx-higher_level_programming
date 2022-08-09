d6a67e0995fa2927e1c8696813a1dae6315eddb0
yakobsolo <yakisolo2009@gmail.com>
2022-08-09 18:24:06 +0000

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,10 +2,7 @@
 -- create a table
 CREATE TABLES second_table(id INT, name VARCHAR(256), score INT);
 -- insert an entity
-INSERT INTO second_table VALUES(1, "John", 10);
--- insert an entity
-INSERT INTO second_table VALUES(2, "Alex", 3);
--- insert a tuple
-INSERT INTO second_table VALUES(3, "Bob", 14);
--- insert a person
-INSERT INTO second_table VALUES(4, "George", 8);
+INSERT INTO second_table VALUES(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
