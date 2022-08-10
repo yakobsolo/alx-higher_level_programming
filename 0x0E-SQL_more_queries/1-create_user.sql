d1d9da193aa3c58f6817cd61a199080df2e120bb
yakobsolo <yakisolo2009@gmail.com>
2022-08-10 09:26:06 +0000

    root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- create user 
 -- create if not exist with password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
