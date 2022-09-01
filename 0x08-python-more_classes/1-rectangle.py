99679c9a39ea24ea479de4d929aa28bbe09f4436
yakobsolo <yakisolo2009@gmail.com>
2022-09-01 09:35:30 +0000

    intialization setter and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -59,5 +59,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if (value < 0):
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
