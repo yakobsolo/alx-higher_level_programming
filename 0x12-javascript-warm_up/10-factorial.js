cba6fe01484464ba6e4af9e8234ea65f25568338
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2022-09-06 17:35:48 +0000

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+function factorial (num) {
+  if (!num || num === 0) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+console.log(factorial(num));
