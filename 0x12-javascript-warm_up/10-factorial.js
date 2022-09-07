a45fc4aabf4ebbcb0963257da57b18655759a2cc
yakobsolo <yakisolo2009@gmail.com>
2022-09-07 05:04:32 +0000

    factorial of number

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const numr = parseInt(process.argv[2]);
 function factorial (numr) {
-  if (!numr|| numr === 0) {
+  if (!numr || numr === 0) {
     return (1);
   } else {
     return (numr * factorial(numr - 1));
