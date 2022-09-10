972cd56d61620473a16cc419c668c3e9f6bd7565
yakobsolo <yakisolo2009@gmail.com>
2022-09-10 17:15:15 +0000

    list city names based on state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+takes in name of state as an arg and lists
+all cities of that state using the db hbtn_0e_4_usa
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states ON\
+    cities.state_id = states.id WHERE states.name LIKE %(state_name)s\
+    ORDER BY cities.id ASC", {'state_name': argv[4]})
+    rows = cur.fetchall()
+    print(', '.join(row[0] for row in rows))
+    db.close()
