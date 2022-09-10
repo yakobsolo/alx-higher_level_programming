b66e8d6e63b25c4495364b2cdd4c627aa2a5c61f
yakobsolo <yakisolo2009@gmail.com>
2022-09-10 09:08:25 +0000

    conecting and listing that start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' lists all states from the database hbtn_0e_0_usa '''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        port=3306,
+        db=argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC;')
+    for row in cur.fetchall():
+        if row[1][0] == 'N':
+            print(row)
+    db.close()
