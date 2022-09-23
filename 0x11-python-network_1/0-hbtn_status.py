86dfd885561332d9bb36dc6c51a2e57542cc48de
yakobsolo <yakisolo2009@gmail.com>
2022-09-23 18:20:19 +0000

    fetches from url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ from urllib.request import urlopen
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
-    with urlopen(url) as resp:
-        result = resp.read()
+    with urlopen(url) as result:
+        output = result.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(result)))
-        print('\t- content: {}'.format(result))
-        print('\t- utf8 content: {}'.format(result.decode('utf-8')))
+        print('\t- type: {}'.format(type(output)))
+        print('\t- content: {}'.format(output))
+        print('\t- utf8 content: {}'.format(output.decode('utf-8')))
