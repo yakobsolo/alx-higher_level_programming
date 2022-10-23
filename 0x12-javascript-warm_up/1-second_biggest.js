81ad97da1c304af5f16cc61e7a780c1ca90fbcba
yakobsolo <yakisolo2009@gmail.com>
2022-10-23 14:51:38 +0000

    updated

--- 0x12-javascript-warm_up/1-second_biggest.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const output = process.argv.map(Number).slice(2).sort((a, b) => a - b).reverse();
-  console.log(output[1]);
-}
